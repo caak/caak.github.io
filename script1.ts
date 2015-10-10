217bc87035963815f7cfe9fc84a852f715a6406b
caak <2005eSBT@gmail.com>
2015-10-10 14:19:15 +0200

    ts to html

--- script1.ts
+++ script1.ts
@@ -1,2 +1,11 @@
-﻿var scriptVar = 'my hidden string!';
-console.log(scriptVar);
+﻿window.onload = function start() {
+
+    var scriptVar = 'my hidden string!';
+    console.log(scriptVar);
+
+    var p = document.createElement('p');
+    var t = document.createTextNode(scriptVar);
+    p.appendChild(t);
+
+    document.body.appendChild(p);
+}
