c067a7eacde0a21417b6fcb876046641c2b2a08f
caak <2005eSBT@gmail.com>
2015-10-18 17:06:08 +0200

    a bit of structure

---
+++ final.js
@@ -0,0 +1,5 @@
+﻿var scriptVar = 'my hidden string!';
+console.log(scriptVar);
+
+var tess = new Tester("Simon", "Bech", "Thougaard");
+console.log(tess.name);
---
+++ script2.js
@@ -0,0 +1,9 @@
+﻿var Tester = (function () {
+    function Tester(firstname, middleinitial, lastname) {
+        this.firstname = firstname;
+        this.middleinitial = middleinitial;
+        this.lastname = lastname;
+        this.name = firstname + " " + middleinitial + " " + lastname;
+    }
+    return Tester;
+})();
